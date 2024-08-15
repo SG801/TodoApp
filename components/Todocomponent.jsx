254528dfe1edde6f69ac0b237864648f1cc1829a
Shilpa <eclectic89@hotmail.com>
2024-08-15 21:16:28 +0100

    Form started

--- components/Todocomponent.jsx
+++ components/Todocomponent.jsx
@@ -1,15 +1,22 @@
-function TODOHero(Taskcompleted, Taskincomplete) {
+/**
+ * Renders a section displaying the number of completed tasks and the total number of tasks.
+ *
+ * @param {number} Taskcompleted - The number of completed tasks.
+ * @param {number} Taskincomplete - The number of incomplete tasks.
+ * @returns {JSX.Element} - A React component that displays the task completion information.
+ */
+export default function TODOHero(Taskcompleted, Taskincomplete) {
   return (
     <>
-    <section>
-      <div>
-        <p>Task Done</p>
-        <p>Keep it up</p>
-      </div>
-    </section>
-      <h1>TODO</h1>
-      <h2>Task completed: {Taskcompleted}</h2>
-      <h2>Task incomplete: {Taskincomplete}</h2>
+      <section>
+        <div>
+          <p>Task Done</p>
+          <p>Keep it up</p>
+        </div>
+        <div>
+          {todos_completed}/{total_todos}
+        </div>
+      </section>
     </>
   );
 
