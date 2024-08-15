d78a07e091a836253f1fed33d345cfb7e4ec28e5
Shilpa <eclectic89@hotmail.com>
2024-08-15 20:57:01 +0100

    header added

---
+++ components/Todocomponent.jsx
@@ -0,0 +1,16 @@
+function TODOHero(Taskcompleted, Taskincomplete) {
+  return (
+    <>
+    <section>
+      <div>
+        <p>Task Done</p>
+        <p>Keep it up</p>
+      </div>
+    </section>
+      <h1>TODO</h1>
+      <h2>Task completed: {Taskcompleted}</h2>
+      <h2>Task incomplete: {Taskincomplete}</h2>
+    </>
+  );
+
+}
